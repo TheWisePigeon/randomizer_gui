7890fb5acb51d20e317e782ea68c0d76d2f96f2d
TheWisePigeon <zozozozeph@gmail.com>
2022-08-20 23:44:05 +0000

    writing landing page

---
+++ src/components/landing.svelte
@@ -0,0 +1,9 @@
+<div class=" h-screen w-full flex justify-center items-center">
+    <div>
+        bruh
+    </div>
+</div>
+
+<script>
+    
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1 class=" bg-red-500">
-    I am supposed to be red
-</h1>
+<script>
+    import Landing from "../components/landing.svelte";
+</script>
+
+<div>
+    <Landing />
+</div>
