088c13579fc806cf9da91176982cb9eed7b8bfe3
TheWisePigeon <zozozozeph@gmail.com>
2022-08-20 23:55:54 +0000

    removed bg url

--- src/components/landing.svelte
+++ src/components/landing.svelte
@@ -1,9 +1,16 @@
-<div class=" h-screen w-full flex justify-center items-center">
-    <div>
+<div class=" h-screen w-full flex justify-center items-center ">
+    <div class="landing bg-red-400">
         bruh
     </div>
 </div>
 
 <script>
-    
+
 </script>
+
+<style>
+    .landing{
+        background: url('static/blob-haikei.svg');
+        background-size: cover;
+    }
+</style>
