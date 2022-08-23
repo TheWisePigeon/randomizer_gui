6ccbce6b024d7f37bbf3f6087e7070021de16bbc
TheWisePigeon <zozozozeph@gmail.com>
2022-08-23 22:35:44 +0000

    feat: changed release version

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,4 +1,4 @@
 <div class=" flex justify-between p-5 items-baseline bg-rose-400">
-    <div class=" text-2xl font-bold">Randomiz<sup class=" text-xs">Beta</sup> </div>
+    <div class=" text-2xl font-bold">Randomiz<sup class=" text-xs">Alpha</sup> </div>
     <div class=" text-lg">About</div>
 </div>
