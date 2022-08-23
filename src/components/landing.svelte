afa4d56c1c6d139bd8f2ba7e134ab66e5631fef3
TheWisePigeon <zozozozeph@gmail.com>
2022-08-23 22:39:24 +0000

    style: hover effect to button

--- src/components/landing.svelte
+++ src/components/landing.svelte
@@ -1,12 +1,12 @@
 
 <div class="  flex justify-center items-center h-screen w-full p-4">
-    <div class=" text-3xl text-center  flex-col justify-between h-3/5 ">
-        <div>
+    <div class=" text-3xl  text-center  flex-col justify-between h-3/5 ">
+        <div >
             Randomiz is a fake user data generator for API testing and prototyping. <br>
             Describe your user data schema and generate up to 1000 rows downlodable as CSV and JSON. <br>
         </div>
         <div>
-            <button class=" rounded-2xl p-3 bg-fuchsia-400 text-white ">
+            <button class=" rounded-2xl p-3 bg-fuchsia-400 text-white hover:bg-white hover:text-fuchsia-400 ">
                 <a href="/generator">Get started</a>
             </button>
         </div>
