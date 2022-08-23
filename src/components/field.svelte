98efe273f82abaad01f0149cd605e7e6f5674d89
TheWisePigeon <zozozozeph@gmail.com>
2022-08-23 23:57:37 +0000

    feat: added store

--- src/components/field.svelte
+++ src/components/field.svelte
@@ -1,10 +1,23 @@
-<script lang="ts">
-  export let name: String;
-  function handleClick(){
-
+<script>
+  import { fields } from "../stores/fields";
+  /**@type {any}*/
+  let chosenFields;
+  fields.subscribe((value) => {
+    chosenFields = value;
+  });
+  export /**
+   * @type {any}
+   */
+  let name;
+  function handleClick() {
+    if(chosenFields.includes(name)){
+        alert('Its in')
+        return
+    }
+    alert('Its not in')
   }
 </script>
 
-<button class="bg-gray-600 rounded-md p-2" on:click="{handleClick}">
+<button class="bg-gray-600 rounded-md p-2" on:click={handleClick}>
   {name}
 </button>
