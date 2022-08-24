858e811cfc7e68d2153be62a1180c8184fbefad8
TheWisePigeon <zozozozeph@gmail.com>
2022-08-24 22:23:01 +0000

    feat: added about and root links

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,4 +1,4 @@
 <div class=" flex justify-between p-5 items-baseline bg-green-400 text-white">
-    <div class=" text-2xl font-bold">Randomiz<sup class=" text-xs">Alpha</sup> </div>
-    <div class=" text-lg">About</div>
+    <div class=" text-2xl font-bold"><a href="/">Randomiz</a><sup class=" text-xs">Alpha</sup> </div>
+    <div class=" text-lg"><a href="/about">About</a></div>
 </div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<div>
+    Randomiz is a fake user data generator for testing and mocking. <br>
+    It can generate up to 100 rows of fake user data. <br>
+    You can choose between 8 fields to describe your users schema <br>
+    And you can download the data as CSV or JSON files. <br>
+    
+</div>
+<div class="absolute bottom-52">
+    Made with Svelte and 💕 by <a href="github.com/TheWisePigeon" class="text-green-400 font-bold underline text-lg">Me</a>
+</div>
