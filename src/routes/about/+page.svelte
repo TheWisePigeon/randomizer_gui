8eeb21606dbb0eb57a38863c4043f1e0542c28c2
TheWisePigeon <zozozozeph@gmail.com>
2022-09-10 18:57:55 +0000

    fix: about section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,7 @@
-<div>
+<div class=" text-center">
     Randomiz is a fake user data generator for testing and mocking. <br>
     It can generate up to 100 rows of fake user data. <br>
     Describe your user schema and generate up to 1000 rows <br>
-    Download your data as CSV or JSON    
 </div>
 <div class="absolute bottom-52">
     Made with Svelte and 💕 by <a href="https://github.com/TheWisePigeon" class="text-green-400 font-bold underline text-lg">TheWisePigeon</a>
