e1772e287456186a0ff253cf3bfe997be7145d93
TheWisePigeon <zozozozeph@gmail.com>
2022-09-10 15:32:11 +0000

    feat: added control on fields name| unicity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,10 @@
   import Field from "../components/field.svelte";
   import { fields } from "../stores/stores";
   import axios from "axios";
-  let fieldsArray:{
-    name: string,
-    type: string
-  }[]
+  let fieldsArray: {
+    name: string;
+    type: string;
+  }[];
 
   fields.subscribe((value) => {
     fieldsArray = value;
@@ -17,29 +17,46 @@
   );
 
   generators.then((value) => {
-    generatorsArray = value.data.generators as ArrayLike<unknown>
+    generatorsArray = value.data.generators as ArrayLike<unknown>;
   });
 
-  let name : string
-  let type : string
-  const addField = ()=>{
-    fields.update(
-        fields=>[...fields, {name: name, type: type }]
-    )
-  }
+  let name: string;
+  let type: string;
+  const addField = () => {
+    for (const field in fieldsArray) {
+      if (fieldsArray[field].name == name) {
+        alert('A field with this name already exists')
+        return false;
+      }
+    }
+    fields.update((fields) => [...fields, { name: name, type: type }]);
+  };
 </script>
+
 <div class=" w-2/5  justify-center h-screen">
   <div class="text-center">Describe your user schema</div>
   <div class=" flex justify-between my-4">
-    <input bind:value="{name}" class=" bg-transparent border-white border-b focus:outline-none" type="text" name="" id="" placeholder="Field" />
+    <input
+      bind:value={name}
+      class=" bg-transparent border-white border-b focus:outline-none"
+      type="text"
+      name=""
+      id=""
+      placeholder="Field"
+    />
     {#if generatorsArray != null}
-    <select bind:value="{type}" class=" bg-transparent border-white border-b" name="type" id="">
-      {#each generatorsArray as generator}
-        <option class=" bg-slate-700" value={generator}>{generator}</option>
-      {/each}
-    </select>
-  {/if}
-    <button on:click="{addField}">
+      <select
+        bind:value={type}
+        class=" bg-transparent border-white border-b"
+        name="type"
+        id=""
+      >
+        {#each generatorsArray as generator}
+          <option class=" bg-slate-700" value={generator}>{generator}</option>
+        {/each}
+      </select>
+    {/if}
+    <button on:click={addField}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
